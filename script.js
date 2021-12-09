536d77be42cc48111a5c01c4e9194281b537ce4a
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-09 11:26:03 +0000

    edit function called blurring

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function blurring() {
   }
   loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
-}
-
-function scale(number, inMin, inMax, outMin, outMax) {
-  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
 //https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
